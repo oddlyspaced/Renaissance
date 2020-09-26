1e59dfcd31596905f50463f5b6ec4d554762f673
oddlyspaced <oddlyspaced1@gmail.com>
2020-09-27 04:54:42 +0530

    MainActivity: Test api client fetching

--- app/src/main/java/com/oddlyspaced/renaissance/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/renaissance/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.oddlyspaced.renaissance
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.oddlyspaced.renaissance.api.ApiClient
+import com.oddlyspaced.renaissance.api.ApiInterface
+import com.oddlyspaced.renaissance.modal.HomeResponse
+import retrofit2.Call
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fetchHome()
+    }
+
+    private fun fetchHome() {
+        // 5 is english, 6 is hindi
+        val language = "5"
+        val client = ApiClient.getApiClient()
+        val apiInterface = client.create(ApiInterface::class.java)
+        apiInterface.fetchLatestHome(language).enqueue(object : retrofit2.Callback<HomeResponse> {
+            override fun onResponse(call: Call<HomeResponse>, response: Response<HomeResponse>) {
+                if (response.isSuccessful) {
+                    Toast.makeText(applicationContext, "${response.body()?.posts?.size} ${response.body()?.slides?.size} ${response.body()?.categories?.size} ${response.body()?.questions?.size}", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onFailure(call: Call<HomeResponse>, t: Throwable) {
+                Toast.makeText(applicationContext, "Failed to fetch posts", Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 }
