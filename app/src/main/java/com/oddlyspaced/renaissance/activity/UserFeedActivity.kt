f59cdf5a5f6474ee2f5999cb8bb1e520c7ae2fb4
oddlyspaced <oddlyspaced1@gmail.com>
2020-09-27 15:22:31 +0530

    UserFeedActivity: Init

---
+++ app/src/main/java/com/oddlyspaced/renaissance/activity/UserFeedActivity.kt
@@ -0,0 +1,12 @@
+package com.oddlyspaced.renaissance.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oddlyspaced.renaissance.R
+
+class UserFeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_feed)
+    }
+}
