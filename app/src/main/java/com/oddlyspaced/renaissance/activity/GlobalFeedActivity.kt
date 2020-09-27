f97c1b8d86d0750584d51bbdb34b31f335ed8296
oddlyspaced <oddlyspaced1@gmail.com>
2020-09-27 18:34:18 +0530

    GlobalFeedActivity: Init

---
+++ app/src/main/java/com/oddlyspaced/renaissance/activity/GlobalFeedActivity.kt
@@ -0,0 +1,12 @@
+package com.oddlyspaced.renaissance.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oddlyspaced.renaissance.R
+
+class GlobalFeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_global_feed)
+    }
+}
