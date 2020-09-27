1b2cace4a6fbc3063414e35c7254fbe5ac18d64a
oddlyspaced <oddlyspaced1@gmail.com>
2020-09-27 12:03:52 +0530

    CategorySelectActivity: Init

---
+++ app/src/main/java/com/oddlyspaced/renaissance/activity/CategorySelectActivity.kt
@@ -0,0 +1,12 @@
+package com.oddlyspaced.renaissance.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oddlyspaced.renaissance.R
+
+class CategorySelectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_select)
+    }
+}
