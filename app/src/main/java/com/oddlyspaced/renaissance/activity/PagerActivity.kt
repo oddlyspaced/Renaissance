33b61f988aa895dde60b96c3f4a40dde794215be
oddlyspaced <oddlyspaced1@gmail.com>
2020-10-01 15:19:30 +0530

    PagerActivity: Setup basic pager activity

--- app/src/main/java/com/oddlyspaced/renaissance/activity/PagerActivity.kt
+++ app/src/main/java/com/oddlyspaced/renaissance/activity/PagerActivity.kt
@@ -2,11 +2,19 @@ package com.oddlyspaced.renaissance.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.oddlyspaced.renaissance.R
+import com.oddlyspaced.renaissance.adapter.PagerAdapter
+import com.oddlyspaced.renaissance.fragment.EmptyFragment
+import kotlinx.android.synthetic.main.activity_pager.*
 
 class PagerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pager)
+
+        val list = arrayListOf<Fragment>(EmptyFragment(), EmptyFragment())
+        val adapter = PagerAdapter(this, list)
+        viewPagerMain.adapter = adapter
     }
 }
