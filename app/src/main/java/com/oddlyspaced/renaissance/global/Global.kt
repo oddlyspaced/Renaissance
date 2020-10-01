d171c7e935aefa7055eed03905717f4f076af73f
oddlyspaced <oddlyspaced1@gmail.com>
2020-10-01 14:46:57 +0530

    Cleanup ignored files

--- app/src/main/java/com/oddlyspaced/renaissance/global/Global.kt
+++
@@ -1,9 +0,0 @@
-package com.oddlyspaced.renaissance.global
-
-class Global {
-    companion object {
-        const val API_URL = "api_url"
-        const val SECURE_KEY = "secure_key"
-        const val ITEM_PURCHASE_CODE = "item_purchase_code"
-    }
-}
