71ee077f3126b28cb963f990dff584ee641c60d6
oddlyspaced <oddlyspaced1@gmail.com>
2020-10-24 02:49:34 +0530

    ProfileFragment: Hide Settings from profile section

--- app/src/main/java/com/oddlyspaced/renaissance/fragment/ProfileFragment.kt
+++ app/src/main/java/com/oddlyspaced/renaissance/fragment/ProfileFragment.kt
@@ -1,11 +1,14 @@
 package com.oddlyspaced.renaissance.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.oddlyspaced.renaissance.R
+import com.oddlyspaced.renaissance.activity.LanguageSelectActivity
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment: Fragment() {
 
@@ -13,4 +16,11 @@ class ProfileFragment: Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        consTopics.setOnClickListener {
+            startActivity(Intent(context!!, LanguageSelectActivity::class.java))
+        }
+    }
 }
